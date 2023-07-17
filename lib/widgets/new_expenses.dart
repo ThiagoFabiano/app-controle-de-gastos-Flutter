a5e3e06ac369338549c37f08bc7a442465e96bd2
Thiago Fabiano <thiagoffabiano@gmail.com>
2023-07-17 15:15:40 -0300

    feat: adiciona input para valor e botão de cancelar ao modal

--- lib/widgets/new_expenses.dart
+++ lib/widgets/new_expenses.dart
@@ -11,10 +11,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -29,11 +31,29 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              prefixText: '\$',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
-              ElevatedButton(onPressed:() {
-                print(_titleController.text);
-              }, child: Text('Save Expense'))
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text('Save Expense'))
             ],
           ),
         ],
