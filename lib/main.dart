2ab9281779d7d0e8f8953cb99569b4c6fc34ed8d
Thiago Fabiano <thiagoffabiano@gmail.com>
2023-07-18 11:15:53 -0300

    feat: adiciona dark-Mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,34 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 96, 59, 181),
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 
 void main() {
   runApp(
     MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
         theme: ThemeData().copyWith(
           useMaterial3: true,
           colorScheme: kColorScheme,
@@ -32,6 +54,7 @@ void main() {
                     fontSize: 16),
               ),
         ),
+        themeMode: ThemeMode.system,
         home: const Expenses()),
   );
 }
