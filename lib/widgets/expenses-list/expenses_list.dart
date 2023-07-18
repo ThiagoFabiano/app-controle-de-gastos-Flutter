353b3c1fe1c4b60d6549e2f7aed2db4f916125b1
Thiago Fabiano <thiagoffabiano@gmail.com>
2023-07-18 10:13:19 -0300

    feat: permite apagar gastos deslizando-os para esquerda ou direita

--- lib/widgets/expenses-list/expenses_list.dart
+++ lib/widgets/expenses-list/expenses_list.dart
@@ -4,20 +4,30 @@ import 'package:flutter/material.dart';
 import '../../models/expense.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({Key? key, required this.expenses}) : super(key: key);
+  const ExpensesList({Key? key, required this.expenses, required this.onRemoveExpense}) : super(key: key);
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       // Provide constraints for the ListView
       constraints: BoxConstraints(
-        maxHeight: MediaQuery.of(context).size.height, // Adjust the height as needed
+        maxHeight:
+            MediaQuery.of(context).size.height, // Adjust the height as needed
       ),
       child: ListView.builder(
         itemCount: expenses.length,
-        itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+        itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(
+            expenses[index],
+          ),
+          onDismissed:(direction) {onRemoveExpense(expenses[index],);} ,
+          child: ExpenseItem(
+            expenses[index],
+          ),
+        ),
       ),
     );
   }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,15 +30,22 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (ctx) => NewExpense(onAddExpense: _addExpense,)
-    );
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) => NewExpense(
+              onAddExpense: _addExpense,
+            ));
   }
 
-  void _addExpense(Expense expense){
+  void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -57,7 +64,10 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+          Expanded(
+            child: ExpensesList(
+                expenses: _registeredExpenses, onRemoveExpense: _removeExpense),
+          ),
         ],
       ),
     );
